ec7aeb02ff33026e42153200b1ceabad8c8da9f1
Nency129 <nencypatel129@gmail.com>
2023-06-07 21:05:55 +0530

    navbar created

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Netflix from './pages/Netflix';
 export default function App() {
   return (
     <BrowserRouter>
-    <Route exact path="/login" element={<Login/>}></Route>
-    <Route exact path="/" element={<Signup/>}></Route>
-    {/* <Route exact path="/" element={<Netflix/>}></Route> */}
+      <Routes>
+        <Route eaxct path="/login" element={<Login />} />
+        <Route eaxct path="/signup" element={<Signup />} />
+        <Route eaxct path="/" element={<Netflix />} />
+      </Routes>
     </BrowserRouter>
   );
 }
