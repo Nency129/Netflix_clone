83071e8d15d8179865663c4580d4fde1c503c862
Nency129 <nencypatel129@gmail.com>
2023-06-12 17:13:58 +0530

    Netflix Slider

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -15,40 +15,40 @@ const initialState = {
 export const getGenres = createAsyncThunk("netflix/genres", async () => {
   const {
     data: { genres },
-  } = await axios.get(`${TMBD_BASE_URL}/genre/movie/list?api_key=${API_KEY}`);
+  } =  await axios.get(`${TMBD_BASE_URL}/genre/movie/list?api_key=${API_KEY}`);
   // console.log(data);
   return genres;
 });
+
 
 const createArrayFromRawData = (array, moviesArray, genres) => {
   array.forEach((movie) => {
     const movieGenres = [];
     movie.genre_ids.forEach((genre) => {
-      const name = genres.find(({ id }) => id === genres);
-      if (name) movieGenres.puch(name.name);
+      const name = genres.find(({ id }) => id === genre);
+      if (name) movieGenres.push(name.name);
     });
-    if (movieGenres.backdrop_path) {
+    if (movie.backdrop_path) 
       moviesArray.push({
         id: movie.id,
-        name: movie.original_name ? movie.original_name : movie.original_title,
+        name: movie?.original_name ? movie.original_name : movie.original_title,
         image: movie.backdrop_path,
         genres: movieGenres.slice(0, 3),
       });
-    }
   });
 };
 
-const getRawData = async (api, genres, paging) => {
+const getRawData = async (api, genres, paging = false) => {
   const moviesArray = [];
   for (let i = 1; moviesArray.length < 60 && i < 10; i++) {
     const {
       data: { results },
     } = await axios.get(`${api}${paging ? `&page=${i}` : ""}`);
     createArrayFromRawData(results, moviesArray, genres);
-
-    return moviesArray;
   }
+  return moviesArray;
 };
+
 
 export const fetchMovies = createAsyncThunk(
   "netflix/trending",
